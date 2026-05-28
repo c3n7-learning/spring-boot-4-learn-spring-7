dbc42884e94471cfed5bb1da7b8e519eca6036e3
Timothy Karani <kartimothy@gmail.com>
2026-05-28 05:35:30 +0300

    section-04: 134-spring boot rest delete employee - coding

--- 04-rest-crud-apis/code/20-spring-boot-rest-crud-employee/src/main/java/com/karani/springboot/cruddemo/rest/EmployeeRestController.java
+++ 04-rest-crud-apis/code/20-spring-boot-rest-crud-employee/src/main/java/com/karani/springboot/cruddemo/rest/EmployeeRestController.java
@@ -82,4 +82,17 @@ public class EmployeeRestController {
 
         return dbEmployee;
     }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
 }
