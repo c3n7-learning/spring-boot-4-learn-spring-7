820e5b6d6a0dd675dc92b89425f3155b943338e5
Timothy Karani <kartimothy@gmail.com>
2026-06-10 04:51:57 +0300

    section-09: 296-@OneToOne Mapping - Coding - Run the Main App

---
+++ 09-jpa-hibernate-advanced-mappings/295-one-to-one-mapping-coding-develop-the-main-app.md
@@ -0,0 +1,3 @@
+# @OneToOne Mapping - Coding - Develop the Main App
+
+Hands On: `09-jpa-hibernate-advanced-mappings/code/01-jpa-one-to-one-uni`
--- 09-jpa-hibernate-advanced-mappings/code/01-jpa-one-to-one-uni/src/main/java/com/karani/cruddemo/CruddemoApplication.java
+++ 09-jpa-hibernate-advanced-mappings/code/01-jpa-one-to-one-uni/src/main/java/com/karani/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.karani.cruddemo;
 
+import com.karani.cruddemo.dao.AppDAO;
+import com.karani.cruddemo.entity.Instructor;
+import com.karani.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +11,45 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CruddemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
         return runner -> {
-            System.out.println("Hello World");
+            createInstructor(appDAO);
         };
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+        /*
+        Instructor tempInstructor = new Instructor("Timothy", "Karani", "timothy@karani.com");
+
+        InstructorDetail tempInstructorDetail =
+                new InstructorDetail(
+                        "http://www.karani.com/youtube",
+                        "Luv 2 Code!!!");
+        */
+
+        Instructor tempInstructor = new Instructor("Madhu", "Patel", "madhu@patel.com");
+
+        InstructorDetail tempInstructorDetail =
+                new InstructorDetail(
+                        "http://www.karani.com/youtube",
+                        "Guitar");
+
+        // associate the objects
+        tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+        // save the instructor
+        //
+        // NOTE: this will ALSO save the details object
+        // because of CascadeType.ALL
+        //
+        System.out.println("Saving instructor: " + tempInstructor);
+        appDAO.save(tempInstructor);
+
+        System.out.println("Done!");
     }
 }
