624361caebde171e1f72aaffba84e39875646dd7
Timothy Karani <kartimothy@gmail.com>
2026-06-25 09:14:10 +0300

    section-09: 330-@OneToMany - UniDirectional - Coding - Retrieve Course and Reviews

---
+++ 09-jpa-hibernate-advanced-mappings/330-one-to-many-unidirectional-coding-retrieve-course-and-reviews.md
@@ -0,0 +1,3 @@
+# @OneToMany - UniDirectional - Coding - Retrieve Course and Reviews
+
+Hands On: `09-jpa-hibernate-advanced-mappings/code/04-jpa-one-to-many-uni`
--- 09-jpa-hibernate-advanced-mappings/code/04-jpa-one-to-many-uni/src/main/java/com/karani/cruddemo/dao/AppDAOImpl.java
+++ 09-jpa-hibernate-advanced-mappings/code/04-jpa-one-to-many-uni/src/main/java/com/karani/cruddemo/dao/AppDAOImpl.java
@@ -136,4 +136,20 @@ public class AppDAOImpl implements AppDAO {
         // Will save the course and associated reviews because of CascadeType.ALL
         entityManager.persist(theCourse);
     }
+
+    @Override
+    public Course findCourseAndReviewsByCourseId(int theId) {
+        // create query
+        TypedQuery<Course> query = entityManager.createQuery(
+                "select c from Course c "
+                      + "JOIN FETCH c.reviews "
+                      + "where c.id = :data", Course.class);
+
+        query.setParameter("data", theId);
+
+        // execute query
+        Course course = query.getSingleResult();
+
+        return course;
+    }
 }
